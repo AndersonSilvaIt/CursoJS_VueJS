d08eb4a48283e1453f603684aac1896492c6b82f
Anderson <anderson@prodwin.com.br>
2020-06-11 12:59:19 -0300

    Fundamentos de Ajax
    Fundamentos de Ajax

---
+++ exercicios-js/ajax/server.js
@@ -0,0 +1,54 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+
+const upload = multer({storage}).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluí­do com sucesso.')
+    })
+})
+
+app.post('/formulario', (req, res) => {
+
+    // aqui eu poderia salvar no banco, mas estou só retornado os dados para o html
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
+app.get('/parOuImpar', (req, res) => {
+    // req.body obetem os dados da requisição
+    // req.query
+    // req.params
+
+    const par = parseInt(req.query.numero) % 2  === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
+app.get('/teste', (req, res) => res.send('ok'))
+app.listen(8080, () => console.log('Executando...'))
